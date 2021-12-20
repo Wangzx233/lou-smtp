fe44d2d1919cd5ebc84d6aa3e79e7151f7e4e657
王鑫 <wangxin@wangxindeMacBook-Air.local>
2021-12-20 17:52:49 +0800

    secret

--- service/email.go
+++ service/email.go
@@ -6,17 +6,17 @@ import (
 )
 
 const (
-	key = "rdkucuzqoflpdjdh"
+	key = "key"
 )
 
 func SendEmail(name,time,reason string) error {
 	e := email.NewEmail()
-	e.From = name+"<2664190827@qq.com>"
-	e.To = []string{"1805982362@qq.com"}
-	e.Cc = []string{"2664190827@qq.com"}
+	e.From = name+"<my@qq.com>"
+	e.To = []string{"to@qq.com"}
+	e.Cc = []string{"my@qq.com"}
 	e.Subject = "请假条"
 	e.Text = []byte("请假人："+name+"\n请假人值班楼栋：知行苑2舍\n请假时间："+time+"\n请假原因："+reason)
 
-	err := e.Send("smtp.qq.com:25", smtp.PlainAuth("", "2664190827@qq.com", key, "smtp.qq.com"))
+	err := e.Send("smtp.qq.com:25", smtp.PlainAuth("", "my@qq.com", key, "smtp.qq.com"))
 	return err
 }
