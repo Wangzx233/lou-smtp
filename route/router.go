53c92c01266d59fd0f042668530c91d3aab79466
王鑫 <wangxin@wangxindeMacBook-Air.local>
2021-12-20 17:32:26 +0800

    cors

--- route/router.go
+++ route/router.go
@@ -4,11 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"lou-smtp/api"
+	"lou-smtp/middelware"
 )
 
 func InitRouter()  {
 	router := gin.Default()
-
+	router.Use(middelware.Cors())
 	router.GET("/email/send",api.SendEmail)
 	err := router.Run(":8080")
 	if err != nil {
